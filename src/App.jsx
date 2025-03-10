ff2ef11a19654881bd391ef1968be978a7c535d0
subtleBatya <batyrsazakow.2003@gmail.com>
2025-03-10 22:25:37 +0300

    fixes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div className='f-2 text-5xl text-amber-200 esas_container'>
+//         HOW ARE YOU
+//       </div>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+import Navbar from "./components/navbar/Navbar";
+
+import Home from "./pages/Home";
+// import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { useEffect, useState } from "react";
+// import ScrollToTopOnRouteChange from "./components/ScrollToTopOnRouteChange";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const location = useLocation();
+  // useEffect(() => {
+  //   // const scrollToTop = () => {
+  //     window.scrollTo({
+  //       top: 0,
+  //       behavior: 'auto' // Optional: smooth scrolling behavior
+  //     });
+  //   // };
+  //   // Scroll to the top when the route changes
+  //   // window.addEventListener('hashchange', scrollToTop);
+  //   // return () => window.removeEventListener('hashchange', scrollToTop);
+  //   });
+    // }, [location.pathname]);
 
   return (
     <>
-      <div className='f-2 text-5xl text-amber-200'>
-        HOW ARE YOU
+      <div className="flex min-h-[100vh] flex-col bg-blue-700/80">
+
+        <Navbar />
+        {/* <Router> */}
+        <div className="grow esas_container">
+
+          {/* <Routes> */}
+            <Home />
+            {/* <ScrollToTopOnRouteChange/> */}
+            {/* <div className="relative"> */}
+            {/* <Route path="ecohub/" exact element={<Home />} /> */}
+            {/* <Route path="/map1" element={<AnotherMap />} /> */}
+
+            {/* <Home /> */}
+            {/* <NewsPage /> */}
+          {/* </Routes> */}
+        </div>
+
+        {/* </Router> */}
+     {/* Su tayda FOOTER Durmaly */}
+        {/* </div> */}
       </div>
+
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Array from '../components/Array'
+// import Navbar from '../components/navbar/Navbar'
+// import Navbar from "./components/Navbar";
+// import RecentBlogs from "./pages/RecentBlogs";
+
+
+
+
+function Home() {
+    return (
+        <div className=''>
+            
+            <Array />
+
+
+           
+
+            {/* <About /> */}
+            {/* <Courses /> */}
+            {/* <RecentBlogs/> */}
+        </div>
+    )
+}
+
+export default Home
